3ca4a8a4dc1e63b1d58a3a9e45403078db344dfb
hursevich <hursevich@gmail.com>
2016-09-09 12:43:56 +0300

    - copyrights added

--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2016 QAPROSOFT (http://qaprosoft.com/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.qaprosoft.carina.demo;
 
 import java.util.List;
@@ -46,13 +61,13 @@ public class WebSampleTest extends UITest
 		HomePage homePage = new HomePage(getDriver());
 		homePage.open();
 		Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-//		// Open model compare page
-//		CompareModelsPage comparePage = homePage.getFooterMenu().openComparePage();
-//		// Compare 3 models
-//		List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7");
-//		// Verify model announced dates
-//		Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
-//		Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2016, March");
-//		Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2016, March");
+		// Open model compare page
+		CompareModelsPage comparePage = homePage.getFooterMenu().openComparePage();
+		// Compare 3 models
+		List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7");
+		// Verify model announced dates
+		Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
+		Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2016, March");
+		Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2016, March");
 	}
 }
