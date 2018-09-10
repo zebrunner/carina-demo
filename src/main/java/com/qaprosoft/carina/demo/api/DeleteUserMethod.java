dc1424628cc82b73dcd1597f053459c5d075106a
Irina Tsvirko <tsvrko@gmail.com>
2018-09-10 17:21:09 +0300

    Update DeleteUserMethod.java

--- src/main/java/com/qaprosoft/carina/demo/api/DeleteUserMethod.java
+++ src/main/java/com/qaprosoft/carina/demo/api/DeleteUserMethod.java
@@ -16,11 +16,11 @@
 package com.qaprosoft.carina.demo.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
 public class DeleteUserMethod extends AbstractApiMethodV2
 {
-
 	public DeleteUserMethod()
 	{
 		super("api/users/_delete/rq.json", "api/users/_delete/rs.json", "api/users/user.properties");
