1d3bed9abde56e7f1d44465929474bfc0ad5511f
Irina Tsvirko <tsvrko@gmail.com>
2018-10-03 14:31:57 +0300

    compilation error is added

--- src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
@@ -31,7 +31,7 @@ import com.qaprosoft.carina.demo.api.PostUserMethod;
  * 
  * @author qpsdemo
  */
-public class APISampleTest extends AbstractTest {
+public class  extends AbstractTest {
     @Test(description = "JIRA#DEMO-0001")
     @MethodOwner(owner = "qpsdemo")
     public void testCreateUser() throws Exception {
