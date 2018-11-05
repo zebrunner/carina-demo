92c8ae6f190409b9b417b788025923f11ecb213c
Irina Tsvirko <tsvrko@gmail.com>
2018-11-05 19:13:42 +0300

    Update APISampleTest.java

--- src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
@@ -57,7 +57,7 @@ public class APISampleTest extends AbstractTest {
 		PostUserMethod api = new PostUserMethod();
 		api.getProperties().remove("name");
 		api.getProperties().remove("username");
-		api.expectResponseStatus(HttpResponseStatusType.CREATED_401);
+		api.expectResponseStatus(HttpResponseStatusType.OK_200);
 		api.callAPI();
 		api.validateResponse();
 	}
