f42d6702198155f330ed9401928c2f91e23a0908
vdelendik <vadim.delendik@gmail.com>
2018-11-14 19:01:36 +0300

    temp branch to keep intermediate work

--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -17,26 +17,23 @@ package com.qaprosoft.carina.demo;
 
 import java.util.List;
 
-import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
-import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
-import com.qaprosoft.carina.core.foundation.utils.tag.TestTag;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
-import com.qaprosoft.carina.core.foundation.dataprovider.annotations.XlsDataSourceParameters;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestTag;
 import com.qaprosoft.carina.demo.gui.components.FooterMenu;
-import com.qaprosoft.carina.demo.gui.components.NewsItem;
 import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs;
 import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs.SpecType;
-import com.qaprosoft.carina.demo.gui.pages.BrandModelsPage;
 import com.qaprosoft.carina.demo.gui.pages.CompareModelsPage;
 import com.qaprosoft.carina.demo.gui.pages.HomePage;
-import com.qaprosoft.carina.demo.gui.pages.ModelInfoPage;
-import com.qaprosoft.carina.demo.gui.pages.NewsPage;
 
 /**
  * This sample shows how create Web test.
@@ -44,29 +41,36 @@ import com.qaprosoft.carina.demo.gui.pages.NewsPage;
  * @author qpsdemo
  */
 public class WebSampleTest extends AbstractTest {
-    @Test(dataProvider = "SingleDataProvider", description = "JIRA#AUTO-0008")
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P3)
-    @TestTag(name = "area test", value = "data provider")
-    @TestTag(name = "specialization", value = "xlsx")
-    @XlsDataSourceParameters(path = "xls/demo.xlsx", sheet = "GSMArena", dsUid = "TUID", dsArgs = "brand, model, display, camera, ram, battery")
-    public void testModelSpecs(String brand, String model, String display, String camera, String ram, String battery) {
-        // Open GSM Arena home page and verify page is opened
-        HomePage homePage = new HomePage(getDriver());
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-        // Select phone brand
-        BrandModelsPage productsPage = homePage.selectBrand(brand);
-        // Select phone model
-        ModelInfoPage productInfoPage = productsPage.selectModel(model);
-        // Verify phone specifications
-        Assert.assertEquals(productInfoPage.readDisplay(), display, "Invalid display info!");
-        Assert.assertEquals(productInfoPage.readCamera(), camera, "Invalid camera info!");
-        Assert.assertEquals(productInfoPage.readRam(), ram, "Invalid ram info!");
-        Assert.assertEquals(productInfoPage.readBattery(), battery, "Invalid battery info!");
-    }
+
+	@BeforeSuite()
+	public void startSuiteDrv() {
+		getDriver();
+	}
+	
+	@BeforeSuite()
+	public void startSuiteDrv2() {
+		getDriver("qwe");
+	}
+
+	
+	@BeforeClass()
+	public void startClassDrv() {
+		getDriver();
+	}
+
+	
+	@BeforeMethod()
+	public void startDrv() {
+		getDriver();
+	}
 
 
+	@Test
+	public void test1() {
+		WebDriver drv = getDriver();
+		LOGGER.info(drv.toString());
+	}
+	 
     @Test(description = "JIRA#AUTO-0009")
     @MethodOwner(owner = "qpsdemo")
     @TestPriority(Priority.P1)
@@ -84,27 +88,9 @@ public class WebSampleTest extends AbstractTest {
         List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
         // Verify model announced dates
         Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
-        Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2016, September");
+        Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June");
         Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
     }
     
-    @Test(description = "JIRA#AUTO-0010")
-    @MethodOwner(owner = "qpsdemo")
-    public void testNewsSearch() {
-        HomePage homePage = new HomePage(getDriver());
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-        
-        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
-        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
-        
-        final String searchQ = "iphone";
-        List<NewsItem> news = newsPage.searchNews(searchQ);
-        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
-        for(NewsItem n : news) {
-            System.out.println(n.readTitle());
-            Assert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ), "Invalid search results!");
-        }
-    }
 
 }
