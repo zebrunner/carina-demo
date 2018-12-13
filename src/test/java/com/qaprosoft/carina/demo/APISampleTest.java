d79d6963bddf409458203ab04229ab9c4b577cd3
tsvrko <tsvrko@gmail.com>
2018-12-13 15:30:54 +0300

    compilation failure added

--- src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
@@ -33,7 +33,7 @@ import com.qaprosoft.carina.demo.api.PostUserMethod;
  *
  * @author qpsdemo
  */
-public class APISampleTest extends AbstractTest {
+public class APISampleTest extends {
 
     @Test(description = "JIRA#DEMO-0001")
     @MethodOwner(owner = "qpsdemo")
