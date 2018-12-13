56a7b13f458a182dbce3ddc174e4ff8d1ba938d6
tsvrko <tsvrko@gmail.com>
2018-12-13 16:04:30 +0300

    test fail initiated

--- src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
@@ -33,7 +33,7 @@ import com.qaprosoft.carina.demo.api.PostUserMethod;
  *
  * @author qpsdemo
  */
-public class APISampleTest extends {
+public class APISampleTest extends AbstractTest {
 
     @Test(description = "JIRA#DEMO-0001")
     @MethodOwner(owner = "qpsdemo")
