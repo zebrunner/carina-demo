b917f286c5ec6b5a5bb2a0663d563ca44c7fc9d4
Maksim Sarychau <msarychau@qaprosoft.com>
2019-01-22 14:04:24 +0100

    Update TagManagerTest.java

--- src/test/java/com/qaprosoft/carina/demo/TagManagerTest.java
+++ src/test/java/com/qaprosoft/carina/demo/TagManagerTest.java
@@ -19,6 +19,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import com.qaprosoft.carina.core.foundation.AbstractTest;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.ITestResult;
@@ -38,7 +39,7 @@ import com.qaprosoft.zafira.models.dto.TagType;
 /**
  * Tests for {@link TagManager}
  */
-public class TagManagerTest {
+public class TagManagerTest extends AbstractTest {
 	protected static final Logger LOGGER = Logger.getLogger(TagManagerTest.class);
 
 	private static final String TAG_NAME = "tag3";
