c6cffa189f92492c1854aeee4fba35bedffb7d9a
vdelendik <vadim.delendik@gmail.com>
2019-07-11 02:08:59 +0300

    updated single driver test to detect during regression testing any failures with test artifacts for such mode

--- src/test/java/com/qaprosoft/carina/demo/WebSampleSingleDriver.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleSingleDriver.java
@@ -15,9 +15,11 @@
  */
 package com.qaprosoft.carina.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
@@ -36,13 +38,19 @@ import com.qaprosoft.carina.demo.gui.pages.HomePage;
  */
 public class WebSampleSingleDriver extends AbstractTest {
     HomePage homePage = null;
+    CompareModelsPage comparePage = null;
+    List<ModelSpecs> specs = new ArrayList<>();
 
+    @BeforeSuite
+    public void startDriver() {
+        // Open GSM Arena home page and verify page is opened
+        homePage = new HomePage(getDriver());
+    }
+    
     @Test
     @MethodOwner(owner = "qpsdemo")
     @TestTag(name = "area test", value = "web")
     public void testOpenPage() {
-        // Open GSM Arena home page and verify page is opened
-        homePage = new HomePage(getDriver());
         homePage.open();
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
     }
@@ -50,18 +58,32 @@ public class WebSampleSingleDriver extends AbstractTest {
     @Test(dependsOnMethods="testOpenPage") //for dependent tests Carina keeps driver sessions by default
     @MethodOwner(owner = "qpsdemo")
     @TestTag(name = "area test", value = "web")
-    public void testCompareModels() {
+    public void testOpenCompare() {
         // Open GSM Arena home page and verify page is opened
         // Open model compare page
         FooterMenu footerMenu = homePage.getFooterMenu();
         Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
-        CompareModelsPage comparePage = footerMenu.openComparePage();
+        comparePage = footerMenu.openComparePage();
+
+    }
+    
+    @Test(dependsOnMethods="testOpenCompare") //for dependent tests Carina keeps driver sessions by default
+    @MethodOwner(owner = "qpsdemo")
+    @TestTag(name = "area test", value = "web")
+    public void testReadSpecs() {
         // Compare 3 models
-        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
+        specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
+    }
+    
+    @Test(dependsOnMethods="testReadSpecs") //for dependent tests Carina keeps driver sessions by default
+    @MethodOwner(owner = "qpsdemo")
+    @TestTag(name = "area test", value = "web")
+    public void testCompareModels() {
         // Verify model announced dates
         Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
         Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June");
         Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
     }
+
     
 }
