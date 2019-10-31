8b2165e0f24f65ae0e54b14d35506afe7b2e188c
ilya-varchenya <ilya.varchenya@gmail.com>
2019-10-31 10:38:09 +0300

    delete vars/

--- vars/test_run.groovy
+++
@@ -1,7 +0,0 @@
-@Library('QPS-Pipeline')
-import com.qaprosoft.jenkins.pipeline.runner.maven.QARunner
-
-def mew() {
-	echo "MEW-MEW-MEW-MEW-MEW"
-	new com.qaprosoft.jenkins.pipeline.runner.maven.QARunner(this).onPush()
-}
