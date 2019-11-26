4fbaeab0b81b3a04804bc48ddd6bb5fa4598b205
Boris Khasid <bkhasid@Boriss-Mac-mini.local>
2019-11-26 17:25:59 +0300

    implemented new test

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
@@ -27,36 +27,113 @@ import com.qaprosoft.carina.demo.gui.components.FooterMenu;
 import com.qaprosoft.carina.demo.gui.components.WeValuePrivacyAd;
 
 public class HomePage extends AbstractPage {
-    Logger LOGGER = Logger.getLogger(HomePage.class);
-    
-    @FindBy(id = "footmenu")
-    private FooterMenu footerMenu;
+	Logger LOGGER = Logger.getLogger(HomePage.class);
 
-    @FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
-    private List<ExtendedWebElement> brandLinks;
+	@FindBy(id = "footmenu")
+	private FooterMenu footerMenu;
 
-    public HomePage(WebDriver driver) {
-        super(driver);
-    }
+	@FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
+	private List<ExtendedWebElement> brandLinks;
 
-    public FooterMenu getFooterMenu() {
-        return footerMenu;
-    }
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
 
-    public BrandModelsPage selectBrand(String brand) {
-        LOGGER.info("selecting '" + brand + "' brand...");
-        for (ExtendedWebElement brandLink : brandLinks) {
-            String currentBrand = brandLink.getText();
-            LOGGER.info("currentBrand: " + currentBrand);
-            if (brand.equalsIgnoreCase(currentBrand)) {
-                brandLink.click();
-                return new BrandModelsPage(driver);
-            }
-        }
-        throw new RuntimeException("Unable to open brand: " + brand);
-    }
-    
-    public WeValuePrivacyAd getWeValuePrivacyAd() {
-    	return new WeValuePrivacyAd(driver);
-    }
+	public FooterMenu getFooterMenu() {
+		return footerMenu;
+	}
+
+	public BrandModelsPage selectBrand(String brand) {
+		LOGGER.info("selecting '" + brand + "' brand...");
+		for (ExtendedWebElement brandLink : brandLinks) {
+			String currentBrand = brandLink.getText();
+			LOGGER.info("currentBrand: " + currentBrand);
+			if (brand.equalsIgnoreCase(currentBrand)) {
+				brandLink.click();
+				return new BrandModelsPage(driver);
+			}
+		}
+		throw new RuntimeException("Unable to open brand: " + brand);
+	}
+
+	public WeValuePrivacyAd getWeValuePrivacyAd() {
+		return new WeValuePrivacyAd(driver);
+	}
+
+	@FindBy(xpath = "//*[@type='button']")
+	private ExtendedWebElement burgerMenuBtn;
+
+	@FindBy(id = "logo")
+	private ExtendedWebElement gsmLogoBtn;
+
+	@FindBy(id = "topsearch-text")
+	private ExtendedWebElement searchLabelBtn;
+
+	@FindBy(xpath = "//*[@class='tip-icon']")
+	private ExtendedWebElement tipUsIcon;
+
+	@FindBy(xpath = "//*[@class='fb-icon']")
+	private ExtendedWebElement fbIcon;
+
+	@FindBy(xpath = "//*[@class='tw-icon']")
+	private ExtendedWebElement twitterIcon;
+
+	@FindBy(xpath = "//*[@class='ig-icon']")
+	private ExtendedWebElement instIcon;
+
+	@FindBy(xpath = "//*[@class='yt-icon']")
+	private ExtendedWebElement youTubeIcon;
+
+	@FindBy(xpath = "//*[@class='rss-icon']")
+	private ExtendedWebElement rssIcon;
+
+	@FindBy(xpath = "//*[@class='login-icon']")
+	private ExtendedWebElement loginIcon;
+
+	@FindBy(xpath = "//*[contains(@class,'signup-icon')]//i")
+	private ExtendedWebElement signUpIcon;
+
+	public boolean checkBurgerMenu() {
+		return burgerMenuBtn.isPresent();
+	}
+
+	public boolean checkLogo() {
+		return gsmLogoBtn.isPresent();
+	}
+
+	public boolean checkSearchLabel() {
+		return searchLabelBtn.isPresent();
+	}
+
+	public boolean checkTipIcon() {
+		return tipUsIcon.isPresent();
+	}
+
+	public boolean checkFbIcon() {
+		return fbIcon.isPresent();
+	}
+
+	public boolean checkTwitterIcon() {
+		return twitterIcon.isPresent();
+	}
+
+	public boolean checkInstagramIcon() {
+		return instIcon.isPresent();
+	}
+
+	public boolean checkYouTubeIcon() {
+		return youTubeIcon.isPresent();
+	}
+
+	public boolean checkRssIcon() {
+		return rssIcon.isPresent();
+	}
+
+	public boolean checkLoginIcon() {
+		return loginIcon.isPresent();
+	}
+
+	public boolean checkSignUpIcon() {
+		return signUpIcon.isPresent();
+	}
 }
--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -44,72 +44,93 @@ import com.qaprosoft.carina.demo.gui.pages.NewsPage;
  * @author qpsdemo
  */
 public class WebSampleTest extends AbstractTest {
-    @Test(dataProvider = "SingleDataProvider", description = "JIRA#AUTO-0008")
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P3)
-    @TestTag(name = "area test", value = "data provider")
-    @TestTag(name = "specialization", value = "xlsx")
-    @XlsDataSourceParameters(path = "xls/demo.xlsx", sheet = "GSMArena", dsUid = "TUID", dsArgs = "brand, model, display, camera, ram, battery")
-    public void testModelSpecs(String brand, String model, String display, String camera, String ram, String battery) {
-        // Open GSM Arena home page and verify page is opened
-        HomePage homePage = new HomePage(getDriver());
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-        
-        //Closing advertising if it's displayed
-        homePage.getWeValuePrivacyAd().closeAdIfPresent();
-        
-        // Select phone brand
-        homePage = new HomePage(getDriver());
-        BrandModelsPage productsPage = homePage.selectBrand(brand);
-        // Select phone model
-        ModelInfoPage productInfoPage = productsPage.selectModel(model);
-        // Verify phone specifications
-        Assert.assertEquals(productInfoPage.readDisplay(), display, "Invalid display info!");
-        Assert.assertEquals(productInfoPage.readCamera(), camera, "Invalid camera info!");
-        Assert.assertEquals(productInfoPage.readRam(), ram, "Invalid ram info!");
-        Assert.assertEquals(productInfoPage.readBattery(), battery, "Invalid battery info!");
-    }
+	@Test(dataProvider = "SingleDataProvider", description = "JIRA#AUTO-0008")
+	@MethodOwner(owner = "qpsdemo")
+	@TestPriority(Priority.P3)
+	@TestTag(name = "area test", value = "data provider")
+	@TestTag(name = "specialization", value = "xlsx")
+	@XlsDataSourceParameters(path = "xls/demo.xlsx", sheet = "GSMArena", dsUid = "TUID", dsArgs = "brand, model, display, camera, ram, battery")
+	public void testModelSpecs(String brand, String model, String display, String camera, String ram, String battery) {
+		// Open GSM Arena home page and verify page is opened
+		HomePage homePage = new HomePage(getDriver());
+		homePage.open();
+		Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
 
+		// Closing advertising if it's displayed
+		homePage.getWeValuePrivacyAd().closeAdIfPresent();
 
-    @Test(description = "JIRA#AUTO-0009")
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P1)
-    @TestTag(name = "area test", value = "web")
-    public void testCompareModels() {
-        // Open GSM Arena home page and verify page is opened
-        HomePage homePage = new HomePage(getDriver());
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-        // Open model compare page
-        FooterMenu footerMenu = homePage.getFooterMenu();
-        Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
-        CompareModelsPage comparePage = footerMenu.openComparePage();
-        // Compare 3 models
-        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
-        // Verify model announced dates
-        Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
-        Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June");
-        Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
-    }
-    
-    @Test(description = "JIRA#AUTO-0010")
-    @MethodOwner(owner = "qpsdemo")
-    public void testNewsSearch() {
-        HomePage homePage = new HomePage(getDriver());
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-        
-        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
-        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
-        
-        final String searchQ = "iphone";
-        List<NewsItem> news = newsPage.searchNews(searchQ);
-        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
-        for(NewsItem n : news) {
-            System.out.println(n.readTitle());
-            Assert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ), "Invalid search results!");
-        }
-    }
+		// Select phone brand
+		homePage = new HomePage(getDriver());
+		BrandModelsPage productsPage = homePage.selectBrand(brand);
+		// Select phone model
+		ModelInfoPage productInfoPage = productsPage.selectModel(model);
+		// Verify phone specifications
+		Assert.assertEquals(productInfoPage.readDisplay(), display, "Invalid display info!");
+		Assert.assertEquals(productInfoPage.readCamera(), camera, "Invalid camera info!");
+		Assert.assertEquals(productInfoPage.readRam(), ram, "Invalid ram info!");
+		Assert.assertEquals(productInfoPage.readBattery(), battery, "Invalid battery info!");
+	}
+
+	@Test(description = "JIRA#AUTO-0009")
+	@MethodOwner(owner = "qpsdemo")
+	@TestPriority(Priority.P1)
+	@TestTag(name = "area test", value = "web")
+	public void testCompareModels() {
+		// Open GSM Arena home page and verify page is opened
+		HomePage homePage = new HomePage(getDriver());
+		homePage.open();
+		Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+		// Open model compare page
+		FooterMenu footerMenu = homePage.getFooterMenu();
+		Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
+		CompareModelsPage comparePage = footerMenu.openComparePage();
+		// Compare 3 models
+		List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5",
+				"Samsung Galaxy J7 Pro");
+		// Verify model announced dates
+		Assert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2015, November");
+		Assert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June");
+		Assert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
+	}
+
+	@Test(description = "JIRA#AUTO-0010")
+	@MethodOwner(owner = "qpsdemo")
+	public void testNewsSearch() {
+		HomePage homePage = new HomePage(getDriver());
+		homePage.open();
+		Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
+
+		NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
+		Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
+
+		final String searchQ = "iphone";
+		List<NewsItem> news = newsPage.searchNews(searchQ);
+		Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
+		for (NewsItem n : news) {
+			System.out.println(n.readTitle());
+			Assert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ), "Invalid search results!");
+		}
+	}
+
+	@Test
+	public void testHeaderBaseElements() {
+		// Open GSM Arena home page and verify page is opened
+		HomePage homePage = new HomePage(getDriver());
+		homePage.open();
+		Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+		Assert.assertTrue(homePage.checkBurgerMenu(), "Burger menu is not present");
+		Assert.assertTrue(homePage.checkLogo(), "Logo is not present");
+		Assert.assertTrue(homePage.checkSearchLabel(), "Search label is not present");
+		Assert.assertTrue(homePage.checkTipIcon(), "Tip icon is not present");
+		Assert.assertTrue(homePage.checkFbIcon(), "Facebook icon is not present");
+		Assert.assertTrue(homePage.checkTwitterIcon(), "Twitter icon is not present");
+		Assert.assertTrue(homePage.checkInstagramIcon(), "Instagram icon is not present");
+		Assert.assertTrue(homePage.checkYouTubeIcon(), "Youtube icon is not present");
+		Assert.assertTrue(homePage.checkRssIcon(), "Rss icon is not present");
+		Assert.assertTrue(homePage.checkLoginIcon(), "Lojin icon is not present");
+		Assert.assertTrue(homePage.checkSignUpIcon(), "SignUp icon is not present");
+
+	}
 
 }
