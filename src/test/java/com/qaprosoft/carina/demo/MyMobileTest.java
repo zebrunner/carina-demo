bb3ca6a28b9b7aba986c62a6e7a95d9f4c161c5e
Yaroslav Hordii <gorduw@Yaroslavs-MacBook-Pro.local>
2021-07-12 20:12:16 +0300

    Fixed convention, name\pass equals and few small issues

--- src/test/java/com/qaprosoft/carina/demo/MyMobileTest.java
+++ src/test/java/com/qaprosoft/carina/demo/MyMobileTest.java
@@ -23,30 +23,30 @@ public class MyMobileTest implements IAbstractTest, IMobileUtils {
 
         //1st step
         LoginPageBase loginPageBase = welcomePageBase.clickNextBtn();
-        Assert.assertTrue(loginPageBase.isPageOpened(), "Login page isn't open.");
+        Assert.assertTrue(loginPageBase.isPageOpened(), "Login Form isn't open.");
 
         //2nd step
-        Assert.assertTrue(loginPageBase.isNamePresented(), "Name field isn't present on the login Page");
-        Assert.assertTrue(loginPageBase.isPassPresented(), "Password field isn't present on the login Page");
-        Assert.assertTrue(loginPageBase.isGenderPresented(), "Male radio isn't present on the login Page");
+        Assert.assertTrue(loginPageBase.isNamePresented(), "Name field isn't present on the Login Form.");
+        Assert.assertTrue(loginPageBase.isPassPresented(), "Password field isn't present on the Login Form.");
+        Assert.assertTrue(loginPageBase.isGenderPresented(), "Male radio isn't present on the Login Form.");
 
         //3rd step
         loginPageBase.typeName(name);
         loginPageBase.typePassword(pass);
-        Assert.assertTrue(loginPageBase.isNameEntered(name), "Name field isn't filled ");
-        Assert.assertTrue(loginPageBase.isPassEntered(pass),"Pass field isn't filled ");
+        Assert.assertTrue(loginPageBase.isNameEntered(name), "Name field isn't filled.");
+        Assert.assertTrue(loginPageBase.isPassEntered(pass),"Pass field isn't filled.");
         //4th step
         loginPageBase.selectMaleSex();
-        Assert.assertTrue(loginPageBase.isMaleSelected(), "Male radio not selected");
+        Assert.assertTrue(loginPageBase.isMaleSelected(), "Male radio not selected.");
 
         //5th step
         loginPageBase.checkPrivacyPolicyCheckbox();
-        Assert.assertTrue(loginPageBase.isPrivacyChecked(), "Privacy button isn't checked");
+        Assert.assertTrue(loginPageBase.isPrivacyChecked(), "Privacy button isn't checked.");
 
         //6th step
-        Assert.assertTrue(loginPageBase.isLoginBtnActive(), "Login button isn't clickable");
+        Assert.assertTrue(loginPageBase.isLoginBtnActive(), "Login button isn't clickable.");
         CarinaDescriptionPageBase carinaPageBase = loginPageBase.clickLoginBtn();
-        Assert.assertTrue(carinaPageBase.isPageOpened(), "Can't log in");
+        Assert.assertTrue(carinaPageBase.isPageOpened(), "Can't log in.");
 
     }
 
