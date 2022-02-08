5513e70afdfc5388965143ad1e9dbff0c4dd295a
kschernopiatova <kchernopiatova@solvd.com>
2022-02-08 13:40:02 +0300

    Annotation-based approach for api testing was implemented

--- src/main/java/com/qaprosoft/carina/demo/api/PostUserMethod.java
+++ src/main/java/com/qaprosoft/carina/demo/api/PostUserMethod.java
@@ -16,9 +16,20 @@
 package com.qaprosoft.carina.demo.api;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
+import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
+import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
+@Endpoint(url = "${base_url}/users", methodType = HttpMethodType.POST)
+@RequestTemplatePath(path = "api/users/_post/rq.json")
+@ResponseTemplatePath(path = "api/users/_post/rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.CREATED_201)
 public class PostUserMethod extends AbstractApiMethodV2 {
+
     public PostUserMethod() {
         super("api/users/_post/rq.json", "api/users/_post/rs.json", "api/users/user.properties");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
