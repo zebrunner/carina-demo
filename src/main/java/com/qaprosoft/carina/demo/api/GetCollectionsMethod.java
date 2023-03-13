5f34386e3ed07ff8eb5937ce48cd0e363fe270e2
yana-glt <galitsynayasya@gmail.com>
2023-03-13 23:47:44 +0100

    moved parameters from endpoint to addParameter method

--- src/main/java/com/qaprosoft/carina/demo/api/GetCollectionsMethod.java
+++ src/main/java/com/qaprosoft/carina/demo/api/GetCollectionsMethod.java
@@ -8,12 +8,16 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.Configuration;
 
-@Endpoint(url = "${base_url}/search/collections?query=krakow&page=1&per_page=1&client_id=oZqNRI9G64U6XQonRp5aZduKHySCnEB55zH_Xuko-ZI", methodType = HttpMethodType.GET)
+@Endpoint(url = "${base_url}/search/collections", methodType = HttpMethodType.GET)
 @ResponseTemplatePath(path = "api/collections/_get/rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetCollectionsMethod extends AbstractApiMethodV2 {
 
     public GetCollectionsMethod(){
+        addParameter("query", "krakow");
+        addParameter("page", "1");
+        addParameter("per_page", "1");
+        addParameter("client_id", "oZqNRI9G64U6XQonRp5aZduKHySCnEB55zH_Xuko-ZI");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url_unsplash"));
 
     }
--- src/main/java/com/qaprosoft/carina/demo/api/GetPhotosUnauthorizedMethod.java
+++ src/main/java/com/qaprosoft/carina/demo/api/GetPhotosUnauthorizedMethod.java
@@ -8,13 +8,16 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.Configuration;
 
-@Endpoint(url = "${base_url}/search/photos?query=krakow&page=1&per_page=1",
+@Endpoint(url = "${base_url}/search/photos",
         methodType = HttpMethodType.GET)
 @ResponseTemplatePath(path = "api/photos/_get/rs_unauthorized.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.UNAUTHORIZED_401)
 public class GetPhotosUnauthorizedMethod extends AbstractApiMethodV2 {
 
     public GetPhotosUnauthorizedMethod() {
+        addParameter("query", "krakow");
+        addParameter("page", "1");
+        addParameter("per_page", "1");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url_unsplash"));
 
     }
