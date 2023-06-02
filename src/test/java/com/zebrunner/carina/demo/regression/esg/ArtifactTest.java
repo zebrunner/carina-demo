c9fa3bc56c8be2cd49e393c7dd4bfc5c23bbc39d
Andrei Kamarouski <akamarouski@solvd.com>
2023-06-02 13:04:49 +0300

    added ESG tests

---
+++ src/test/java/com/zebrunner/carina/demo/regression/esg/ArtifactTest.java
@@ -0,0 +1,69 @@
+package com.zebrunner.carina.demo.regression.esg;
+
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.demo.gui.pages.common.HomePageBase;
+import com.zebrunner.carina.demo.utils.ArtifactUtils;
+import com.zebrunner.carina.utils.R;
+import com.zebrunner.carina.utils.report.ReportContext;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+import java.util.List;
+
+public class ArtifactTest implements IAbstractTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void artifactTest() {
+        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+        LOGGER.info("Checking the presence of artifacts (video.log, task.log)...");
+        List<String> artifacts = List.of("video.log", "task.log");
+        SoftAssert softAssert = new SoftAssert();
+        for (String artifact : artifacts) {
+            boolean isPresent = ArtifactUtils.isArtifactPresent(
+                    new FluentWait<>(getDriver())
+                            .pollingEvery(Duration.ofSeconds(1))
+                            .withTimeout(Duration.ofSeconds(30)),
+                    artifact);
+            softAssert.assertTrue(isPresent, artifact + " not found.");
+            if (isPresent) {
+                LOGGER.info("Artifact '{}' is present.", artifact);
+                boolean isNotEmpty = FileUtils.sizeOf(ReportContext.getArtifact(getDriver(), artifact)) > 0;
+                if (isNotEmpty) {
+                    LOGGER.info("Artifact '{}' is not empty.", artifact);
+                }
+                softAssert.assertTrue(isNotEmpty, String.format("Artifact '%s' is empty.", artifact));
+            }
+        }
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void mitmDumpArtifactTest() {
+        R.CONFIG.put("proxy_type", "Zebrunner", true);
+        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+        LOGGER.info("Checking the presence of the mitm.dump artifact...");
+        Assert.assertTrue(ArtifactUtils.isArtifactPresent(new FluentWait<>(getDriver())
+                                .pollingEvery(Duration.ofSeconds(1))
+                                .withTimeout(Duration.ofSeconds(30)),
+                        "mitm.dump"),
+                "Artifact 'mitm.dump' was not found.");
+        LOGGER.info("Artifact 'mitm.dump' is present.");
+        Assert.assertTrue(FileUtils.sizeOf(ReportContext.getArtifact(getDriver(), "mitm.dump")) > 0,
+                "Artifact 'mitm.dump' is empty.");
+        LOGGER.info("Artifact 'mitm.dump' is not empty.");
+    }
+}
