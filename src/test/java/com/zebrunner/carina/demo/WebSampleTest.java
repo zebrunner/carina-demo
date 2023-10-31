e5fab37beb9ed11bd747549065a34ef0d37b125c
Andrei Kamarouski <akamarouski@solvd.com>
2023-10-31 15:47:38 +0100

    changes

--- src/test/java/com/zebrunner/carina/demo/WebSampleTest.java
+++ src/test/java/com/zebrunner/carina/demo/WebSampleTest.java
@@ -20,9 +20,11 @@ import java.util.List;
 import com.zebrunner.carina.demo.gui.components.ModelItem;
 import com.zebrunner.carina.demo.gui.components.NewsItem;
 import com.zebrunner.carina.demo.gui.components.compare.ModelSpecs;
+import com.zebrunner.carina.webdriver.config.WebDriverConfiguration;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -45,7 +47,13 @@ import com.zebrunner.carina.core.registrar.tag.TestPriority;
  */
 public class WebSampleTest implements IAbstractTest {
 
-    @Test
+
+    @BeforeSuite
+    public void beforeSuite() {
+        WebDriverConfiguration.addIgnoredNewSessionErrorMessages("timed out waiting for a node to become available", "TimeoutException");
+    }
+
+    @Test(invocationCount = 200, threadPoolSize = 100)
     @MethodOwner(owner = "qpsdemo")
     @TestPriority(Priority.P3)
     @TestLabel(name = "feature", value = { "web", "regression" })
@@ -65,81 +73,6 @@ public class WebSampleTest implements IAbstractTest {
         softAssert.assertEquals(productInfoPage.readCamera(), "50MP", "Invalid camera info!");
         softAssert.assertEquals(productInfoPage.readRam(), "3-8GB RAM", "Invalid ram info!");
         softAssert.assertEquals(productInfoPage.readBattery(), "5000mAh", "Invalid battery info!");
-        softAssert.assertAll();
-    }
-
-    @Test
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P1)
-    @TestLabel(name = "feature", value = { "web", "acceptance" })
-    public void testCompareModels() {
-        // Open GSM Arena home page and verify page is opened
-        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-        // Open model compare page
-        CompareModelsPageBase comparePage = homePage.openComparePage();
-        // Compare 2 (for mobile testing) or 3 (for desktop testing) models
-        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy S23 Ultra", "Samsung Galaxy J7 Pro");
-        // Verify model announced dates
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(specs.get(0).readSpec(ModelSpecs.SpecType.ANNOUNCED), "2016, March 31. Released 2016, May 06");
-        softAssert.assertEquals(specs.get(0).readSpec(ModelSpecs.SpecType.TECHNOLOGY), "GSM / HSPA / LTE");
-        softAssert.assertEquals(specs.get(1).readSpec(ModelSpecs.SpecType.ANNOUNCED), "2023, February 29");
-        softAssert.assertEquals(specs.get(1).readSpec(ModelSpecs.SpecType.TECHNOLOGY), "GSM / CDMA / HSPA / EVDO / LTE / 5G");
-        // for desktop could be compared 3 devices, when for mobile only 2
-        if (specs.size() > 2) {
-            softAssert.assertEquals(specs.get(2).readSpec(ModelSpecs.SpecType.ANNOUNCED), "2017, June");
-            softAssert.assertEquals(specs.get(2).readSpec(ModelSpecs.SpecType.TECHNOLOGY), "GSM / HSPA / LTE");
-        }
-
-        softAssert.assertAll();
-    }
-
-    @Test
-    @MethodOwner(owner = "qpsdemo")
-    @TestLabel(name = "feature", value = { "web", "acceptance" })
-    public void testNewsSearch() {
-        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-
-        NewsPageBase newsPage = homePage.getFooterMenu().openNewsPage();
-        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
-
-        final String searchQ = "iphone";
-        List<NewsItem> news = newsPage.searchNews(searchQ);
-        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
-        SoftAssert softAssert = new SoftAssert();
-        for (NewsItem n : news) {
-            System.out.println(n.readTitle());
-            softAssert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ),
-                    "Invalid search results for " + n.readTitle());
-        }
-        softAssert.assertAll();
-    }
-
-    @Test()
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P3)
-    @TestLabel(name = "feature", value = { "web", "regression" })
-    public void testBrandGroup() {
-        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-
-        AllBrandsPageBase allBrandsPage = homePage.openAllBrandsPage();
-        Assert.assertTrue(allBrandsPage.isPageOpened(), "All mobile phone brands page is not opened!");
-
-        final String brandName = "Lava";
-        BrandModelsPageBase brandModelsPage = allBrandsPage.selectBrand(brandName);
-        List<ModelItem> models = brandModelsPage.getModels();
-        SoftAssert softAssert = new SoftAssert();
-        for (ModelItem modelItem : models) {
-            softAssert.assertFalse(modelItem.readModel().contains(brandName),
-                    "Model " + modelItem.readModel() + " should not include brand " + brandName + " in title");
-        }
-
         softAssert.assertAll();
     }
 
