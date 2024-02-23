294d66d2c00b4cff7e8a2a5638f1b935d7c11775
Andrei Kamarouski <akamarouski@solvd.com>
2024-02-23 18:00:38 +0100

    changes

---
+++ src/test/java/com/zebrunner/carina/demo/WebSampleTestTest.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright 2020-2023 Zebrunner Inc (https://www.zebrunner.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.zebrunner.carina.demo;
+
+import java.util.List;
+
+import com.zebrunner.agent.core.registrar.Label;
+import com.zebrunner.carina.demo.gui.components.ModelItem;
+import com.zebrunner.carina.demo.gui.components.NewsItem;
+import com.zebrunner.carina.demo.gui.components.compare.ModelSpecs;
+import com.zebrunner.carina.utils.config.Configuration;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.zebrunner.carina.demo.gui.pages.common.AllBrandsPageBase;
+import com.zebrunner.carina.demo.gui.pages.common.BrandModelsPageBase;
+import com.zebrunner.carina.demo.gui.pages.common.CompareModelsPageBase;
+import com.zebrunner.carina.demo.gui.pages.common.HomePageBase;
+import com.zebrunner.carina.demo.gui.pages.common.ModelInfoPageBase;
+import com.zebrunner.carina.demo.gui.pages.common.NewsPageBase;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import com.zebrunner.carina.core.registrar.tag.Priority;
+import com.zebrunner.carina.core.registrar.tag.TestPriority;
+
+/**
+ * This sample shows how create Web test.
+ *
+ * @author qpsdemo
+ */
+public class WebSampleTestTest implements IAbstractTest {
+
+    @Test
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P3)
+    @TestLabel(name = "feature", value = { "web", "regression" })
+    public void testLabel() {
+        Configuration.get("label_name").ifPresent(label -> {
+            Label.attachToTestRun(label, Configuration.getRequired("label_value"));
+        });
+        // Open GSM Arena home page and verify page is opened
+        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+        // Select phone brand
+        BrandModelsPageBase productsPage = homePage.selectBrand("Samsung");
+        // Select phone model
+        ModelInfoPageBase productInfoPage = productsPage.selectModel("Galaxy A04");
+        // Verify phone specifications
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(productInfoPage.readDisplay(), "6.5\"", "Invalid display info!");
+        softAssert.assertEquals(productInfoPage.readCamera(), "50MP", "Invalid camera info!");
+        softAssert.assertEquals(productInfoPage.readRam(), "3-8GB RAM", "Invalid ram info!");
+        softAssert.assertEquals(productInfoPage.readBattery(), "5000mAh", "Invalid battery info!");
+        softAssert.assertAll();
+    }
+
+}
